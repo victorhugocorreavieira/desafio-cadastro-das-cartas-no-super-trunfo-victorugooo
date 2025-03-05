e177d4377cbff769d763cdbb20da5c3f2bb5e042
victorugooo <victorhugovieira2100@gmail.com>
2025-03-05 20:04:34 +0000

    updatefinal

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,64 +1,74 @@
 #include <stdio.h>
 
-  int main () { 
+int main ()
+{
 
-    
+
     char cidade[10],cidade2[10],estado[4],estado2[4];
     char code[] = "01";
     char codigo[] = "02";
-    float area , area2, pib , pib2;
-    int pontotur , pontotur2 , populacao , populacao2;
+    float area, area2, pib, pib2, densidadepopulacional, densidadepopulacional2, pibpercapita, pibpercapita2;
+    int pontotur, pontotur2, populacao, populacao2;
 
-printf("digite a sua cidade: ");
-scanf ("%s"  , cidade);
+    printf("digite a sua cidade: ");
+    scanf ("%s", cidade);
 
-printf("digite o seu estado: ");
-scanf("%s"   ,estado);
+    printf("digite o seu estado: ");
+    scanf("%s",estado);
 
-printf("digite a sua população: ");
-scanf("%d" , &populacao);
+    printf("digite a sua populacao: ");
+    scanf("%d", &populacao);
 
-printf("digite a area: ");
-scanf("%f"  , &area );
+    printf("digite a area: ");
+    scanf("%f", &area );
 
-printf("digite o pib: ");
-scanf("%f"  ,&pib);
- 
-printf("digite a quantidade de pontos turistiscos: ");
-scanf("%d"  , &pontotur );
+    printf("digite o pib: ");
+    scanf("%f",&pib);
 
-printf("a sua cidade: %s \n", cidade);
-printf("o seu estado: %s \n",estado);
-printf("população: %d \n " , populacao);
-printf("area: %f  \n ",area);
-printf("pontos turisticos %d \n " , pontotur );
-printf("pib  %f \n  "  , pib );
+    printf("digite a quantidade de pontos turistiscos: ");
+    scanf("%d", &pontotur );
 
-printf("digite a sua cidade: \nn ");
-scanf ("%s"  ,cidade2);
+    densidadepopulacional = (float)(populacao / area);
+    pibpercapita = (float)(pib / populacao);
 
-printf("digite o seu estado: \nn");
-scanf ("%s"   ,estado2);
+    printf("a sua cidade: %s \n", cidade);
+    printf("o seu estado: %s \n",estado);
+    printf("população: %d \n ", populacao);
+    printf("area: %f  \n ",area);
+    printf("pontos turisticos %d \n ", pontotur );
+    printf("pib  %f \n  ", pib );
+    printf("pib percapta e : %f \n", pibpercapita);
+    printf("densidade populacional e : %f \n\n", densidadepopulacional);
 
-printf("digite a sua populacao: \nn");
-scanf ("%d"  , populacao2);
+    printf("digite a sua cidade: ");
+    scanf ("%s",cidade2);
 
-printf("digite a area: \nn");
-scanf ("%f"  , &area2);
+    printf("digite o seu estado: ");
+    scanf ("%s",estado2);
 
-printf("digite o pib: \nn ");
-scanf ("%f"   ,&pib2);
+    printf("digite a sua populacao: ");
+    scanf ("%d", &populacao2);
 
-printf("digite a quantidade de pontos turisticos: \nn ");
-scanf ("%d"   ,&pontotur2);
+    printf("digite a area: ");
+    scanf ("%f", &area2);
 
-printf("cidade: %s \nn" , cidade);
-printf(" estado: %s \nn" , estado);
-printf("populacao: %d  \nn "  , populacao);
-printf("area: %f  \nn " , area);
-printf("pontosturisticos: \nn ", pontotur );
-printf ("pib: %f \nn  "  , pib );
+    printf("digite o pib: ");
+    scanf ("%f",&pib2);
 
-return 0; 
+    printf("digite a quantidade de pontos turisticos: ");
+    scanf ("%d",&pontotur2);
+
+    densidadepopulacional2 = (float)(populacao2 / area2);
+    pibpercapita2 = (float)(pib2 / populacao2);
+
+    printf("cidade: %s \n", cidade2);
+    printf(" estado: %s \n", estado2);
+    printf("populacao: %d  \n ", populacao2);
+    printf("area: %f  \n ", area2);
+    printf("pontosturisticos: %d \n ", pontotur2);
+    printf ("pib: %f \n  ", pib2);
+    printf("pibpercapta é : %f \n", pibpercapita2);
+    printf("densidade populacional é : %f \n",densidadepopulacional2);
+
+    return 0;
 }
- 
