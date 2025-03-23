0fcf8925a2b810d494cfe38497bf5b7cdb240780
victorhugocorreavieira <rugov4536@gmail.com>
2025-03-23 02:47:53 +0000

    tema1etema2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main()
     char cidade1[200],cidade2[200],estado1[10],estado2[10];
     char codigo1[] = "01";
     char codigo2[] = "02";
-    int populacao1, populacao2, pontosturisticos1, pontosturisticos2;
+    int populacao1, populacao2,pontosturisticos1,pontosturisticos2;
     float area1,area2,pib1,pib2,densidadepopulacional1,densidadepopulacional2,pibpercapta1,pibpercapta2;
     double superpoder1,superpoder2;
 
@@ -27,7 +27,7 @@ int main()
     printf("digite a area da sua cidade:");
     scanf("%f", &area1);
 
-    printf("digite o pib da sua cidade:\n");
+    printf("digite o pib da sua cidade:");
     scanf("%f" , &pib1);
 
     //calcule densidade populacional e pibpercapta da primeira carta
@@ -67,7 +67,7 @@ int main()
 
     //exibir o superpoder das duas cartas
     printf("\ncarta1 %s%s tem superpoder %.3f! \n", estado1, codigo1, superpoder1);
-    printf("carta2 %s%s tem superpoder %.3f!\n", estado2, codigo2, superpoder2);
+    printf("\ncarta2 %s%s tem superpoder %.3f!\n", estado2, codigo2, superpoder2);
 
     //exibe todos os atributos da primeira carta
 
@@ -75,42 +75,51 @@ int main()
     printf("Cidade: %s \n", cidade1);
     printf("Estado: %s \n", estado1);
     printf("Populacao: %d \n", populacao1);
-    printf("Ponto(s) turístico(s): %d \n", pontosturisticos1);
+    printf("Ponto(s) turistico(s): %d \n", pontosturisticos1);
     printf("Area: %.2f km² \n", area1);
-    printf("PIB: %.2f bilhões de reais \n", pib1);
-    printf("Densidade populacional é: %.3f hab/km² \n", densidadepopulacional1);
+    printf("PIB: %.2f bilhoes de reais \n", pib1);
+    printf("Densidade populacional e: %.3f hab/km² \n", densidadepopulacional1);
     printf("PIB percapta: %.3f reais \n", pibpercapta1);
     printf("Superpoder: %.3f \n\n\n", superpoder1);
 
     //exibe todos atributos cadastrados da segunda carta
 
-    printf("\nO código da segunda cidade é: %s%s \n", estado2,codigo2);
+    printf("\nO código da segunda cidade e: %s%s \n", estado2,codigo2);
     printf("Cidade: %s \n", cidade2);
     printf("Estado: %s \n", estado2);
     printf("Populacao: %d \n", populacao2);
-    printf("Pontos turísticos: %d \n", pontosturisticos2);
+    printf("Pontos turisticos: %d \n", pontosturisticos2);
     printf("Área: %.2f km² \n", area2);
-    printf("PIB: %.2f bilhões de reais \n", pib2);
-    printf("Densidade populacional é: %.3f hab/km² \n", densidadepopulacional2);
+    printf("PIB: %.2f bilhoes de reais \n", pib2);
+    printf("Densidade populacional e: %.3f hab/km² \n", densidadepopulacional2);
     printf("PIB percapta: %.3f reais \n", pibpercapta2);
     printf("Superpoder: %.3f\n", superpoder2);
 
     //compara os atributos das cartas qual é maior e exibe 1 para verdadeiro e 0 para falso
 
-    printf("\na população da cidade %s%s é maior do que da cidade %s%s? %i \n", estado1, codigo1,
+    printf("\na populaçao da cidade %s%s e maior do que da cidade %s%s? %i \n", estado1, codigo1,
            estado2, codigo2, (populacao1 > populacao2));
-    printf("A cidade %s%s tem mais pontos turísticos do que da cidade %s%s? %i \n", estado1, codigo1,
+    printf("A cidade %s%s tem mais pontos turisticos do que da cidade %s%s? %i \n", estado1, codigo1,
            estado2, codigo2, (pontosturisticos1 > pontosturisticos2));
-    printf("A área da cidade %s%s é maior do que a área da cidade %s%s? %i \n", estado1, codigo1,
+    printf("A area da cidade %s%s é maior do que a area da cidade %s%s? %i \n", estado1, codigo1,
            estado2, codigo2, (area1 > area2));
-    printf("O PIB da cidade %s%s é maior do que o PIB da cidade %s%s? %i \n", estado1, codigo1,
+    printf("O PIB da cidade %s%s e maior do que o PIB da cidade %s%s? %i \n", estado1, codigo1,
            estado2, codigo2, (pib1 > pib2));
-    printf("A densidade populacional da cidade %s%s é maior do que a densidade populacional da cidade %s%s? %i \n"
+    printf("A densidade populacional da cidade %s%s e maior do que a densidade populacional da cidade %s%s? %i \n"
            , estado1, codigo1, estado2, codigo2, (densidadepopulacional1 > densidadepopulacional2));
-    printf("O PIB percapta da cidade %s%s é maior do que o PIB percapta da cidade %s%s? %i \n", estado1, codigo1,
+    printf("O PIB percapta da cidade %s%s e maior do que o PIB percapta da cidade %s%s? %i \n", estado1, codigo1,
            estado2, codigo2, (pibpercapta1 > pibpercapta2));
 
-    return 0;
+    //bloco de codigo a ser executado se a condicao for verdadeira
+
+    if(area1 > area2){
+    printf("a area1 e maior que a area2 \n");
+
+    //bloco de codigo a ser executado se a condicao for falsa
+
+    }else{
+    printf("a area1 e menor que a area2 \n");
+    }
 }
 
 
